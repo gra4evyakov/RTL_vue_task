d8a511474d8be452f8e4616bd059773e45acbf87
gra4evyakov <gra4evyakov@yandex.ru>
2023-09-26 12:19:47 +0300

    add dialog logic

--- src/components/Main/VMain.vue
+++ src/components/Main/VMain.vue
@@ -1,22 +1,48 @@
 <template>
   <main class="main">
-    <v-main-inventory :cells="25" :inventory="inventory.bag" @toggle-navigation="navigationToggle"/>
-    <v-main-navigation v-if="navigationStatus" />
+    <v-main-inventory
+      :cells="25"
+      :inventory="inventory.bag"
+      @toggle-navigation="navigationToggle"
+    />
+    <v-main-navigation 
+      v-if="navigationStatus" 
+      @close="navigationToggle"
+    >
+      <ui-card 
+        :item="inventory.currentItem" 
+        @delete-item="dialogToggle" 
+      />
+      <ui-dialog 
+        v-if="dialogStatus" 
+        @close="dialogToggle" 
+        @submit="inventory.removeItem" 
+      />
+    </v-main-navigation>
   </main>
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue';
-import { useInventoryStore } from '@/stores/inventory';
+import { ref } from 'vue'
+import { useInventoryStore } from '@/stores/inventory'
 
 import VMainInventory from '@/components/Main/VMainInventory.vue'
-import VMainNavigation from './VMainNavigation.vue';
+import VMainNavigation from './VMainNavigation.vue'
 
-const navigationStatus = ref(false)
+import uiCard from '../_ui/uiCard.vue'
+import UiDialog from '../_ui/uiDialog.vue'
+
 const inventory = useInventoryStore()
+const navigationStatus = ref(false)
+const dialogStatus = ref(false)
 
-const navigationToggle = () => {
+const navigationToggle = (id: number) => {
+  if(id) inventory.changeCurrentId(id)
   navigationStatus.value = !navigationStatus.value
+}
+
+const dialogToggle = () => {
+  dialogStatus.value = !dialogStatus.value
 }
 </script>
 
@@ -24,6 +50,8 @@ const navigationToggle = () => {
 .main {
   position: relative;
   grid-area: Main;
+
+  overflow: hidden;
 
   outline: var(--border);
   border-radius: var(--border-radius);
