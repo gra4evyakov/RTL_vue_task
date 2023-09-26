1274cf2fd6594c6ac8b086fab54ea9d739f203e1
gra4evyakov <gra4evyakov@yandex.ru>
2023-09-26 12:19:17 +0300

    add uiDialog and uiCard components

---
+++ src/components/_ui/uiCard.vue
@@ -0,0 +1,80 @@
+<template>
+    <div class="card">
+        <div class="card-image">
+            <svgItem size="large" :color="item.color"/>
+        </div>
+        <div class="card-content">  
+            <uiSkeleton />
+        </div>
+        <div class="card-action">
+            <uiButton @click="deleteItem" color="danger">Удалить предмет</uiButton>
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+import svgItem from '../_icons/svgItem.vue';
+import uiButton from './uiButton.vue';
+import uiSkeleton from './uiSkeleton.vue';
+
+const emits = defineEmits(['deleteItem']);
+
+interface InventoryItem {
+  id: number
+  color: string
+  quantity: number,
+  position: number
+}
+
+const { item } = defineProps<{
+    item: InventoryItem
+}>()
+
+const deleteItem = () => {
+    emits('deleteItem');
+}
+</script>
+
+<style scoped>
+.card {
+    padding: 15px 15px 18px 15px;
+    height: 100%;
+
+    display: grid;
+    grid-template-rows: min-content 1fr min-content;
+    row-gap: 16px;
+}
+
+.card-image {
+    margin: 0 auto;
+    padding: 40px 0 15px;
+}
+
+.card-content {
+    position: relative;
+    height: 100%;
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+
+.card-content::before {
+    position: absolute;
+    top: 0;
+    width: 100%;
+    height: 1px;
+    content: '';
+    display: block;
+    background-color: var(--border-color);
+}
+.card-content::after {
+    position: absolute;
+    bottom: 0;
+    width: 100%;
+    height: 1px;
+    content: '';
+    display: block;
+    background-color: var(--border-color);
+}
+</style>
---
+++ src/components/_ui/uiDialog.vue
@@ -0,0 +1,58 @@
+<template>
+    <form class="dialog" @submit.prevent="submit(inputValue)">
+        <input class="dialog-input" v-model="inputValue" placeholder="Введите количество" type="text" required>
+        <div class="dialog-actions">
+            <uiButton @click="close">Отмена</uiButton>
+            <uiButton type="submit" color="danger">Подтвердить</uiButton>
+        </div>
+    </form> 
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+import uiButton from './uiButton.vue';
+
+const emits = defineEmits(['close', 'submit']);
+
+const inputValue = ref('')
+
+const close = () => {
+    emits('close')
+}
+
+const submit = (value : string) => {
+    emits('submit', value)
+    close()
+}
+</script>
+
+<style scoped>
+.dialog {
+    position: absolute;
+    bottom: 0;
+
+    display: grid;
+    grid-template-rows: 1fr 1fr;
+    row-gap: 20px;
+
+    width: 100%;
+    padding: 18px;
+    backdrop-filter: blur(15px);
+    background: var(--bg-section);
+
+    border-top: var(--border);
+}
+
+.dialog-input {
+    padding: 11px;
+    border: var(--border);
+    border-radius: 4px;
+    background: var(--bg-section);
+    color: inherit;
+}
+
+.dialog-actions {
+    display: flex;
+    gap: 10px;
+}
+</style>
