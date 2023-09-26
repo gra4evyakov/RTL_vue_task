bd1c26b2b3fc2adcbcbd253e46ac1ddf3d976a91
gra4evyakov <gra4evyakov@yandex.ru>
2023-09-26 14:09:33 +0300

    navigation logic

--- src/components/Main/VMainNavigation.vue
+++ src/components/Main/VMainNavigation.vue
@@ -1,24 +1,40 @@
 <template>
-    <nav class="navigation">
-
-    </nav>
+  <nav class="navigation">
+    <slot />
+    <div class="actions">
+      <uiButton icon @click="emits('close')">
+        <template #icon>
+          <svgClose />
+        </template>
+      </uiButton>
+    </div>
+  </nav>
 </template>
 
 <script setup lang="ts">
+import svgClose from '../_icons/svgClose.vue'
+import uiButton from '../_ui/uiButton.vue'
 
+const emits = defineEmits(['close'])
 </script>
 
 <style scoped>
 .navigation {
-    position: absolute;
-    top: 0;
-    right: 0;
-    width: 250px;
-    height: 100%;
+  position: absolute;
+  top: 0;
+  right: 0;
+  width: 250px;
+  height: 100%;
 
-    border-left: var(--border);
-    border-top-right-radius: var(--border-radius);
-    border-bottom-right-radius: var(--border-radius);
-    backdrop-filter: blur(15px);
+  border-left: var(--border);
+  border-top-right-radius: var(--border-radius);
+  border-bottom-right-radius: var(--border-radius);
+  backdrop-filter: blur(15px);
+}
+
+.actions {
+  position: absolute;
+  right: 8px;
+  top: 8px;
 }
 </style>
